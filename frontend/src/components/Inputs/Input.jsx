d1f4528af05b6059304c8544f91282571aa4dcbd
Aditya Udai <adityaudai.1305@gmail.com>
2026-05-04 10:14:05 +0530

    password input added

--- frontend/src/components/Inputs/Input.jsx
+++ frontend/src/components/Inputs/Input.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {FaRegEye,FagRegEyeSlash} from "react-icons/fa6";
 
-export const Input = () => {
+export const Input = ({value,onChange,placeholder,label,type}) => {
+  const [showPassword,setShowPassword]=useState(false);
+  const toggleShowPassword=()=>{
+    setShowPassword(!showPassword);
+  };
   return (
-    <div>Input</div>
+    <div>
+      <label className='text-[13px] text-slate-800'>{label}</label>
+      <div className='input-box'>
+        <input 
+        type={type=='password' ? showPassword ?'text':'password':type}
+        placeholder={placeholder}
+        className='w-full bg-transparent outline-none'
+        value={value}
+        onChange={(e)=onChange(e)}
+        />
+
+        {type==="password" &&(
+          <>
+          {showPassword ?(
+            <FaRegEye
+            size={22}
+            className='text-primary cursor-pointer'
+            onClick={()=>toggleShowPassword()} />
+          ):(
+            <FagRegEyeSlash
+            size={2}
+            className='text-slate-400 cursor-pointer'
+            onClick={()=>toggleShowPassword()}/>
+          )
+          }
+          </>
+        )}
+      </div>
+    </div>
   )
 }
