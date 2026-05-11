2ec80ea1ceb9f554deb5d5c81480fea78aa36575
Aditya Udai <adityaudai.1305@gmail.com>
2026-05-11 12:01:17 +0530

    Signup email , username and password lenght checking added

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,67 +1,125 @@
-import React ,{use, useState}from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Input } from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
 import { ProfilePhotoSelector } from '../../components/Inputs/ProfilePhotoSelector';
 
 export const SignUp = () => {
-  const [profilePic,setProfilePic]=useState(null);
-  const [fullName,setFullName]=useState("");
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
-  const [error,setError]=useState(null);
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
-  const handleSignUp=async (e)=>{
-  }
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!email) {
+      setError("Enter email address");
+      return;
+    }
+    if (!fullName) {
+      setError("Enter your full name");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Enter password");
+      return;
+    }
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters");
+      return;
+    }
+    setError("");
+  };
+
   return (
     <AuthLayout>
       <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
-        <h3 className='text-xl font-semibold text-black'>Create Account</h3>
-        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Enter your details below </p>
+
+        <h3 className='text-xl font-semibold text-black'>
+          Create Account
+        </h3>
+
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Enter your details below
+        </p>
 
         <form onSubmit={handleSignUp}>
 
-          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
-
+          <ProfilePhotoSelector
+            image={profilePic}
+            setImage={setProfilePic}
+          />
 
           <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-            <Input 
-            value={fullName}
-            onChange={({target})=>setFullName(target.value)}
-            label="Full Name"
-            placeholder="Your Name"
-            type="text"/>
-            <Input 
-            value={email}
-            onChange={({target})=>setEmail(target.value)}
-            label="Email Address"
-            placeholder="Your Email"
-            type="email"/>
+
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="Your Name"
+              type="text"
+            />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="Your Email"
+              type="email"
+            />
+
             <div className='col-span-2'>
-          <Input 
-          value={password}
-          onChange={({target})=>setPassword(target.value)}
-          label="Password"
-          placeholder="Min 8 character"
-          type="password"/>
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Min 8 character"
+                type="password"
+              />
+            </div>
+
           </div>
-          </div>
-                  {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
-                  <button type='submit' className='w-full bg-blue-600 text-white py-3 rounded-lg mt-4 hover:bg-blue-700 transition'>
-                    SignUp
-                  </button>
-                  <p className='text-[-13px] text-slate-800 mt-3'>
-                    Have an acoount {""}
-                    <Link className='font--medium text-primary underline' to="/login">
-                    Login
-                    </Link>
-                  </p>
+
+          {error && (
+            <p className='text-red-500 text-xs pb-2.5'>
+              {error}
+            </p>
+          )}
+
+          <button
+            type='submit'
+            className='w-full bg-blue-600 text-white py-3 rounded-lg mt-4 hover:bg-blue-700 transition'
+          >
+            SignUp
+          </button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Have an account{" "}
+            <Link
+              className='font-medium text-primary underline'
+              to="/login"
+            >
+              Login
+            </Link>
+          </p>
+
         </form>
       </div>
     </AuthLayout>
   )
 }
+
 export default SignUp
