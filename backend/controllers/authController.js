540a6b44194eb9facce833c85923b42885ed5e60
Aditya Udai <adityaudai.1305@gmail.com>
2026-05-15 13:02:36 +0530

    Login function to verify registered user with email and password added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,7 +41,24 @@ exports.registerUser=async (req,res)=>{
 
 //Login User
 exports.loginUser=async(req,res)=>{
-    
+    const {email,password}=req.body;
+    // Check for missing fields
+    if(!email || !password){
+        return res.status(400).json({message:"Please fill in all required fields"});
+    }
+    try{
+        const user=await User.findOne({email});
+        if (!user ||!(await user.comparePassword(password))){
+            return res.status(401).json({message:"Invalid email or password"});
+        }
+        res.status(200).json({
+            id:user._id,
+            user,
+            token:generateToken(user._id),
+        });
+    }catch(err){
+        res.status(500).json({message:"Error logging in user",error:err.message});
+    }
 };
 
 //Get User Info
