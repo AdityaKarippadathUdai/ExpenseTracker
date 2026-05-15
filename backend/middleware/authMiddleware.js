7e89aa10bb1d6524d0a4e0ba448e4a069d9f68f3
Aditya Udai <adityaudai.1305@gmail.com>
2026-05-15 13:31:54 +0530

    authmiddleware added to handle jwt verification usng jwt secret and verify userid password

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt=require("jsonwebtoken");
+const User=require("../models/User");
+
+exports.protect=async(req,res,next)=>{
+    let token=req.headers.authorization?.split(" ")[1];
+    if(!token) return res.status(401).json({message:"No token provided, authorization denied"});
+
+    try{const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        req.user=await User.findById(decoded.id).select("-password");
+        if(!req.user) return res.status(401).json({message:"User not found, authorization denied"});
+        next();
+    }catch(err){
+        res.status(401).json({message:"Invalid token, authorization denied",error:err.message});
+    }
+};
