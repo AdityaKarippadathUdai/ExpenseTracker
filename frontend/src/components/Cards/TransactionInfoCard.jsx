02b9cb4582af9a64298de572732228510ab9dc6d
Aditya Udai <adityaudai.1305@gmail.com>
2026-05-28 16:16:09 +0530

    Transaction Info card to information about recent transactions completed , with different color  to show spending and income

--- frontend/src/components/Cards/TransactionInfoCard.jsx
+++ frontend/src/components/Cards/TransactionInfoCard.jsx
@@ -15,6 +15,9 @@ const TransactionInfoCard = ({
     type,
     hideDeleteBtn
 }) => {
+    const getAmountStyles=()=>{
+        return type==="income" ? "bg-green-50 text-green-500" : "bg-red-50 text-red-500";
+    }
   return (
     <div className='group relative flex items-center gap-4 mt-2 p-3rounded-lg hover:bg-gray-100'>
         <div className='w-12 h-12 flex items-center justify-center text-xl text-gray-800 bg-gray-100 rounded-full'>
@@ -24,23 +27,23 @@ const TransactionInfoCard = ({
                 <LuUUtensils />
             )}
         </div>
-        <div clasName=''>
+        <div clasName='flex-1 flex items-center justify-between'>
             <div>
-                <p className=''>{title}</p>
-                <p className=''>{date}</p>
+                <p className='text-sm text-gray-700 font-medium'>{title}</p>
+                <p className='text-xs text-gray-500 mt-1'>{date}</p>
             </div>
 
-            <div className=''>
+            <div className='flex items-center gap-2'>
                 {!hideDeleteBtn && (
-                    <button className=''
+                    <button className='text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity'
                     onClick={onDelete}
                     >
                         <LuTrash2 size={18} />
                     </button>
                 )}
                 <div
-                className={`flex items-center gap-2 px-3 py-1.5 rounded-md`}>
-                    <h6 className=''>
+                className={`flex items-center gap-2 px-3 py-1.5 rounded-md ${getAmountStyles()}`}>
+                    <h6 className='text-xs fiont-medium'>
                         {type==="income" ? "+": "-"}${amount}
                     </h6>
                     {type==="income"?<LuTrendingUp/>: <LuTrendingDown/>}
