2aa01723c18fe89f6664e7e29a2aea9fe64591be
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-03 22:42:18 +0530

    feat: Add Financial overview component with piechart

---
+++ frontend/src/components/Dashboard/FinanceOverview.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import CustomePieChart from '../Charts/CustomePieChart';
+
+const COLORS=["#875CF5","#FA2C37","#FF6900"];
+
+const FinanceOverview = ({totalBalance,totalIncome,totalExpense}) => {
+    const balanceData=[
+        {name:"Total Balance",
+            amount: totalBalance
+        },
+        {name:"Total Income",
+            amount: totalIncome
+        },
+        {name:"Total Expense",
+            amount: totalExpense
+        }
+    ]
+
+  return (
+    <div className='card'>
+        <div className='flex items-center justify-between'>
+            <h5 className='text-lg'>Financial Overview</h5>
+        </div>
+
+        <CustomePieChart
+        data={balanceData}
+        label="Total Balance"
+        totalAmount={`$${totalBalance}`}
+        colors={COLORS}
+        showTextAnchor
+        />
+
+    </div>
+  )
+}
+
+export default FinanceOverview
