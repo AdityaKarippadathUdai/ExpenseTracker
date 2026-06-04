85ed9227bfeb3d12a5b6e9ae030cdecd120bd1f7
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-04 23:15:49 +0530

    Updated custom piechart

--- frontend/src/components/Charts/CustomePieChart.jsx
+++ frontend/src/components/Charts/CustomePieChart.jsx
@@ -8,63 +8,57 @@ import {
     ResponsiveContainer
 } from 'recharts';
 
-const CustomePieChart = ({
-    data,
-    label,
-    totalAmount,
-    colors,
-    showTextAnchor
-}) => {
-  return (
-    <ResponsiveContainer width="100%" height={380}>
-        <PieChart>
-            <Pie
-               data={data}
-               dataKey="amount"
-                nameKey="name"
-                cx="50%"
-                cy="50%"
-                outerRadius={130}
-                innerRadius={100}
-                labelLine={false}
-            >
-                {data.map((entry, index) => (
-                    <Cell
-                        key={`cell-${index}`}
-                        fill={colors[index % colors.length]}
-                    />
-                ))}
-            </Pie>
-            <Tooltip />
-            <Legend />
-            {showTextAnchor &&(
-                <>
-                <text
-                x="50%"
-                y="50%"
-                dy={-25}
-                textAnchor="middle"
-                fill="#666"
-                fontSize="14px"
+const CustomePieChart = ({ data, label, totalAmount, colors, showTextAnchor }) => {
+    return (
+        <ResponsiveContainer width="100%" height={380}>
+            <PieChart>
+                <Pie
+                    data={data}
+                    dataKey="amount"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={130}
+                    innerRadius={100}
+                    labelLine={false}
                 >
-                    {label}
-                </text>
-                <text
-                x="50%"
-                y="50%"
-                dy={8}
-                textAnchor="middle"
-                fill="#333"
-                fontSize="24px"
-                fontWeight="semi-bold"
-                >
-                    {totalAmount}
-                </text>
-                </>
-            )}
-        </PieChart>
-    </ResponsiveContainer>
-  )
-}
+                    {data.map((entry, index) => (
+                        <Cell
+                            key={`cell-${index}`}
+                            fill={colors[index % colors.length]}
+                        />
+                    ))}
+                </Pie>
+                <Tooltip />
+                <Legend />
+                {showTextAnchor && (
+                    <text
+                        x="50%"
+                        y="50%"
+                        dy={-25}
+                        textAnchor="middle"
+                        fill="#666"
+                        fontSize={14}
+                    >
+                        {label}
+                    </text>
+                )}
+                {showTextAnchor && (
+                    <text
+                        x="50%"
+                        y="50%"
+                        dy={8}
+                        textAnchor="middle"
+                        fill="#333"
+                        fontSize={24}
+                        fontWeight="bold"
+                    >
+                        {totalAmount}
+                    </text>
+                )}
+            </PieChart>
+        </ResponsiveContainer>
+    );
+};
 
-export default CustomePieChart
+export default CustomePieChart;
