7e60f913edac4f070e1521584db814097af25ec5
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-05 23:50:19 +0530

    Recent income with chart added

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 import { useUserAuth } from '../../hooks/useUserAuth';
@@ -13,6 +14,7 @@ import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
 import Last30DaysExpenses from '../../components/Dashboard/Last30DaysExpenses';
+import RecentIncomeWithChart from '../../components/Dashboard/RecentIncomeWithChart';
 
 export const Home = () => {
   useUserAuth();
@@ -82,15 +84,20 @@ export const Home = () => {
           totalExpense={dashboardData?.totalExpenses || 0}
            /> */}
 
-          <ExpenseTransactions
+          {/* <ExpenseTransactions
           transactions={dashboardData?.last30DaysExpenses?.transactions || []}
           onSeeMore={()=>navigate("/expense")}
           />
 
           <Last30DaysExpenses
           data={dashboardData?.last30DaysExpenses?.transactions || []}
-            />
+            /> */}
 
+          <RecentIncomeWithChart
+          data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
+          totalIncome={dashboardData?.totalIncome || 0}
+          />
+          
         </div>
       </div>
     </DashboardLayout>
