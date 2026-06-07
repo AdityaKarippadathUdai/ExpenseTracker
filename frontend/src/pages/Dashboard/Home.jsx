7d75c8b447898bfc8a33e53296425b998c2b0fad
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-07 23:08:57 +0530

    Home modified

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -51,7 +51,7 @@ export const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto ">
-        {/* <div  className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+       <div  className='grid grid-cols-1 md:grid-cols-3 gap-6'>
           <InfoCard
           icon={<IoMdCard/>}
           label="Total Balance"
@@ -72,9 +72,9 @@ export const Home = () => {
           value={addThousandSeparator(dashboardData?.totalExpenses || 0)}
           color="bg-red-500"
           />
-        </div> */}
+        </div> 
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
-        {/*  <RecentTransactions 
+          <RecentTransactions 
           transactions={dashboardData?.recentTransactions || []}
           onSeeMore={()=>navigate("/expense")}
           />
@@ -83,16 +83,16 @@ export const Home = () => {
           totalBalance={dashboardData?.totalBalance || 0}
           totalIncome={dashboardData?.totalIncome || 0}
           totalExpense={dashboardData?.totalExpenses || 0}
-           /> */}
+           /> 
 
-          {/* <ExpenseTransactions
+           <ExpenseTransactions
           transactions={dashboardData?.last30DaysExpenses?.transactions || []}
           onSeeMore={()=>navigate("/expense")}
           />
 
           <Last30DaysExpenses
           data={dashboardData?.last30DaysExpenses?.transactions || []}
-            /> */}
+            /> 
 
           <RecentIncomeWithChart
           data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
