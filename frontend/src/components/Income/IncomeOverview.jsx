eded7ab49b33077464061fc4d3d94525ae2747e1
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-07 23:54:05 +0530

    Implemented Income Overview to add income and show barchart

--- frontend/src/components/Income/IncomeOverview.jsx
+++ frontend/src/components/Income/IncomeOverview.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {LuPlus} from 'react-icons/lu';
+import CustomBarChart from '../Charts/CustomBarChart';
+import { useEffect } from 'react';
+import { prepareIncomeBarChartData } from '../../utils/helper';
 
-const IncomeOverview = ({transactions}) => {
+const IncomeOverview = ({transactions,onAddIncome}) => {
+    const [chartData,setChartData] = useState([]);
+
+    useEffect(()=>{
+        const result=prepareIncomeBarChartData(transactions);
+        setChartData(result);
+        return ()=>{};
+    },[transactions]);
   return (
-    <div>IncomeOverview</div>
+    <div className='card'>
+        <div className='flex items-center justify-between'>
+        <div className=''>
+            <h5 clasName='text-lg'>Income Overview</h5>
+            <p className='text-xs text-gray-400 mt-0.5'>
+                Track your income and stay on top of your financial goals .
+            </p>
+        </div>
+        <button className='add-btn' onClick={onAddIncome}>
+            <LuPlus className='text-lg'/>
+            AddIncome
+        </button>
+    </div>
+    <div className='mt-10'>
+        <CustomBarChart data={chartData}/>
+    </div>
+    </div>
   )
 }
 
