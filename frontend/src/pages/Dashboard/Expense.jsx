5643b96de4feb7dfc0baf83c82273e322712b050
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-16 23:51:33 +0530

    feat :Add export of expense data in form of xlsx

--- frontend/src/pages/Dashboard/Expense.jsx
+++ frontend/src/pages/Dashboard/Expense.jsx
@@ -88,7 +88,30 @@ export const Expense = () => {
   };
 
   // handle download Expense data
-  const handleDownloadExpenseDetails = async () => {};
+  const handleDownloadExpenseDetails = async () => {
+
+    try{
+      const response=await axiosInstance.get(
+        API_PATHS.EXPENSE.DOWNLOAD_EXPENSE,
+        {
+          responseType:"blob"
+        }
+      );
+      const url=window.URL.createObjectURL(new Blob([response.data]));
+      const link=document.createElement('a');
+
+      link.href=url;
+      link.setAttribute("download","expense_details.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    }catch(error){
+      console.error("Error downloading expense details",error);
+      toast.erro("Failed to download expense details please try again later");
+    }
+
+  };
 
 
 
